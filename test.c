f2faf4b56bcb87ce35b4eda9a61032502c815501
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-06-30 19:16:00 -0300

    Test: Credito

---
+++ test.c
@@ -0,0 +1,7 @@
+#include "test_credito.h"
+
+int main()
+{
+    test_credito();
+    return 0;
+}
---
+++ test_credito.h
@@ -0,0 +1,8 @@
+#ifndef TEST_CREDITO_H_INCLUDED
+#define TEST_CREDITO_H_INCLUDED
+
+void test_credito_es_nulo();
+
+void test_credito();
+
+#endif // TEST_CREDITO_H_INCLUDED
