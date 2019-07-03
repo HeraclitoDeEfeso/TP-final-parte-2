dd48dff253cdf01cb3147f4d9a047137f03245b6
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-03 17:48:28 -0300

    Clave: clave Apellido-DNI interfaz

---
+++ claves.h
@@ -0,0 +1,28 @@
+#ifndef CLAVES_H_INCLUDED
+#define CLAVES_H_INCLUDED
+#include "cliente.h"
+
+/**
+ * typedef Clave: TAD que representa la Clave de un Cliente
+ * @cliente: enlace al Cliente en caso de modificación (no es clave).
+ * @posicion: posición del registro Cliente en el archivo (no es clave).
+ * @edad: edad del Cliente y clave secundaria.
+ * @apellido: apellido del Cliente y clave primaria.
+ *
+ * Este TAD será utilizado para representar la Clave de un Cliente
+ * en el Indice y por consecuencia el tipo de los argumentos para la
+ * función comparadora en los métodos del AVL y en las funciones de
+ * persistencia y recuperación del Indice.
+ */
+typedef struct {
+    Cliente *cliente;
+    int posicion;
+    int edad;
+    char *apellido;
+} Clave;
+
+int compararClave(void *clave1, void *clave2);
+Clave *recuperarClave(FILE *indice);
+int persistirClave(Clave *clave, FILE *indice);
+
+#endif // CLAVES_H_INCLUDED
