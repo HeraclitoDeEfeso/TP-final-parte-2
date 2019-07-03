59da3e824d893e87632cdbb72c2284fad741be1b
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-03 12:16:10 -0300

    Test: Credito completo header

--- test_credito.h
+++ test_credito.h
@@ -2,7 +2,12 @@
 #define TEST_CREDITO_H_INCLUDED
 
 void test_credito_es_nulo();
-
+void test_credito_es_nulo();
+void test_credito_no_es_nulo();
+void test_no_pagar_credito_nulo();
+void test_no_pagar_credito_saldado();
+void test_no_pagar_credito_demas();
+void test_pagar_credito();
 void test_credito();
 
 #endif // TEST_CREDITO_H_INCLUDED
