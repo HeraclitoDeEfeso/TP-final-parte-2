1aeac1d29d5d98e9a8da8dbc8eff50d3500f613b
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-04 22:40:10 -0300

    Aplicacion: completo bosquejo modulo alta Cliente

--- aplicacion.c
+++ aplicacion.c
@@ -1,10 +1,16 @@
 #include "aplicacion.h"
 #include "cliente.h"
+#include "indice.h"
+#include "claves.h"
 
 void altaCliente()
 {
-    int posicionArchivo;
+    FILE *base; // suponemos que está abierta para escribir
+    int posicionArchivo; // suponemos que está seteado
+    Indice *indice; // suponemos que es el correcto
     Cliente *nuevoCliente = crearCliente();
+    guardarCliente(nuevoCliente, base);
+    agregarClaveIndice(indice, crearClave(nuevoCliente, posicionArchivo), &compararClave);
 }
 
 int main(){}
