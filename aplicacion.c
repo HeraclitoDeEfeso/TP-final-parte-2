4c1f8adb18bcec7a7b78f69a7a0a95d952d3ddae
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-04 22:25:49 -0300

    Aplicacion: limpio e inicio alta de Cliente

--- aplicacion.c
+++ aplicacion.c
@@ -1,40 +1,10 @@
-#include <stdio.h>
-#include <strings.h>
-#include <stdlib.h>
 #include "aplicacion.h"
+#include "cliente.h"
 
-int compararIndice(void *clave1, void *clave2)
-{
-    return (int) clave1 - (int) clave2;
-}
 void altaCliente()
 {
     int posicionArchivo;
     Cliente *nuevoCliente = crearCliente();
 }
 
-int main(){
-    char *opciones[] = {"Alta de Cliente",
-                        "Buscar Clientes",
-                        "Listar Clientes",
-                        "Alta de Crédito",
-                        "Listar Créditos",
-                        "Operar Crédito",
-                        "Cancelar credito",
-                        "Salir."};
-    int i;
-    Aplicacion *app = malloc(sizeof(Aplicacion));
-    /** case1 Alta cliente **/
-    Cliente *nuevo =  malloc(sizeof(Cliente));
-    nuevo = formularioCliente(nuevo);
-    while(&app->nuevos[i] != 0){
-        i= i+1;
-    }
-    app->nuevos[i] = *nuevo;
-    app->indices = agregarClaveIndice(app->indices, (void*) app->nuevos[i].nombre , &compararIndice);
-    app->indices = agregarClaveIndice(app->indices, (void*) app->nuevos[i].edad , &compararIndice);
-
-    //asumiendo que se guardan los vivos al final/exit
-
-}
-
+int main(){}
--- aplicacion.h
+++ aplicacion.h
@@ -1,24 +1,4 @@
-
 #ifndef __APLICACION__
 #define __APLICACION__
-#include "cliente.h"
-#include "credito.h"
-#include "indice.h"
-#include "aplicacion.h"
-#define CLIENTES_NUEVOS 20
-//include de los paquetes Avl y menu
-
-typedef struct {
-	FILE base;
-	Cliente nuevos[CLIENTES_NUEVOS];
-	Indice *indices;
-	int persistidos;
-	int siguienteID;
-} Aplicacion;
-
-int seleccionarCredito(Credito credito[]);
-int seleccionarOperacion(Credito *credito);
-int formularioMonto(Credito *credito);
-int formularioClaves(enum clave, char minimo[], char maximo[] );
 
 #endif //__APLICACION__
