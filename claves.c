888663bdda564938fbb8bd872572ced13240d70d
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-06 05:30:33 -0300

    Claves: implementacion de dos claves

--- claves.c
+++ claves.c
@@ -3,40 +3,90 @@
 #include <stdlib.h>
 #include "claves.h"
 
-int compararClave(void *clave1, void *clave2)
+int compararClaveNombreDNI(void *clave1, void *clave2)
 {
-    Clave *c1 = (Clave*) clave1;
-    Clave *c2 = (Clave*) clave2;
+    ClaveNombreDNI *c1 = (ClaveNombreDNI*) clave1;
+    ClaveNombreDNI *c2 = (ClaveNombreDNI*) clave2;
     int comparacion = strcmp(c1->nombre, c2->nombre);
     return (comparacion == 0)? c1->dni - c2->dni : comparacion;
 }
 
-int recuperarClave (void **clave, FILE *archivo)
+int recuperarClaveNombreDNI(void **clave, FILE *archivo)
 {
-    *clave = malloc(sizeof(Clave));
-    return fread(*clave, sizeof(Clave), 1, archivo);
+    *clave = malloc(sizeof(ClaveNombreDNI));
+    return fread(*clave, sizeof(ClaveNombreDNI), 1, archivo);
 }
 
-int persistirClave(void *clave, FILE *archivo)
+int persistirClaveNombreDNI(void *clave, FILE *archivo)
 {
-    return fwrite(clave, sizeof(Clave), 1, archivo);
+    return fwrite(clave, sizeof(ClaveNombreDNI), 1, archivo);
 }
 
-Clave *crearClave(Cliente *cliente, long posicionArchivo)
+void *crearClaveNombreDNI(Cliente *cliente, long posicionArchivo)
 {
-    Clave *nuevaClave = malloc(sizeof(Clave));
+    ClaveNombreDNI *nuevaClave = malloc(sizeof(ClaveNombreDNI));
     nuevaClave->posicion = posicionArchivo;
     nuevaClave->dni = cliente->dni;
     strncpy(nuevaClave->nombre, cliente->nombre, 15);
     return nuevaClave;
 }
 
-Clave *formularioClave()
+void formularioClaveNombreDNI(void **destino)
 {
-    Clave *clave = calloc(sizeof(Clave), 1);
+    ClaveNombreDNI *clave = calloc(sizeof(ClaveNombreDNI), 1);
+    printf("\nDatos del Cliente a buscar\n");
     printf("Nombre del cliente: ");
     scanf("%s", clave->nombre);
-    printf("DNI: ");
+    printf("               DNI: ");
     scanf("%i", &(clave->dni));
-    return clave;
+    *destino = clave;
+}
+
+int obtenerPosicionClaveNombreDNI(void *clave)
+{
+    return ((ClaveNombreDNI*) clave)->posicion;
+}
+
+int compararClaveEdadDNI(void *clave1, void *clave2)
+{
+    ClaveEdadDNI *c1 = (ClaveEdadDNI*) clave1;
+    ClaveEdadDNI *c2 = (ClaveEdadDNI*) clave2;
+    int comparacion = c1->edad - c2->edad;
+    return (comparacion == 0)? c1->dni - c2->dni : comparacion;
+}
+
+int recuperarClaveEdadDNI(void **clave, FILE *archivo)
+{
+    *clave = malloc(sizeof(ClaveEdadDNI));
+    return fread(*clave, sizeof(ClaveEdadDNI), 1, archivo);
+}
+
+int persistirClaveEdadDNI(void *clave, FILE *archivo)
+{
+    return fwrite(clave, sizeof(ClaveEdadDNI), 1, archivo);
+}
+
+void *crearClaveEdadDNI(Cliente *cliente, long posicionArchivo)
+{
+    ClaveEdadDNI *nuevaClave = malloc(sizeof(ClaveEdadDNI));
+    nuevaClave->posicion = posicionArchivo;
+    nuevaClave->dni = cliente->dni;
+    nuevaClave->edad = cliente->edad;
+    return nuevaClave;
+}
+
+void formularioClaveEdadDNI(void **destino)
+{
+    ClaveEdadDNI *clave = calloc(sizeof(ClaveEdadDNI), 1);
+    printf("\nDatos del Cliente a buscar\n");
+    printf("Edad del cliente: ");
+    scanf("%i", &(clave->edad));
+    printf("             DNI: ");
+    scanf("%i", &(clave->dni));
+    *destino = clave;
+}
+
+int obtenerPosicionClaveEdadDNI(void *clave)
+{
+    return ((ClaveEdadDNI*) clave)->posicion;
 }
--- claves.h
+++ claves.h
@@ -2,12 +2,13 @@
 #define CLAVES_H_INCLUDED
 #include <stdio.h>
 #include "cliente.h"
+#include "indice.h"
 
 /**
- * typedef Clave: TAD que representa la Clave de un Cliente
+ * typedef ClaveNombreDNI: representa la Clave Nombre más DNI de un Cliente
  * @posicion: posición del registro Cliente en el archivo (no es clave).
- * @edad: edad del Cliente y clave secundaria.
- * @apellido: apellido del Cliente y clave primaria.
+ * @dni: DNI del Cliente y clave secundaria.
+ * @nombre: nombre del Cliente y clave primaria.
  *
  * Este TAD será utilizado para representar la Clave de un Cliente
  * en el Indice y por consecuencia el tipo de los argumentos para la
@@ -15,15 +16,44 @@
  * persistencia y recuperación del Indice.
  */
 typedef struct {
-    long posicion;
+    int posicion;
     int dni;
     char nombre[15];
-} Clave;
+} ClaveNombreDNI;
 
-int compararClave(void *clave1, void *clave2);
-int recuperarClave (void **clave, FILE *archivo);
-int persistirClave(void *clave, FILE *indice);
-Clave *crearClave(Cliente *cliente, long posicionArchivo);
-Clave *formularioClave();
+/**
+ * typedef ClaveEdadDNI: representa la Clave Edad más DNI de un Cliente
+ * @posicion: posición del registro Cliente en el archivo (no es clave).
+ * @dni: DNI del Cliente y clave secundaria.
+ * @nombre: nombre del Cliente y clave primaria.
+ *
+ * Este TAD será utilizado para representar la Clave de un Cliente
+ * en el Indice y por consecuencia el tipo de los argumentos para la
+ * función comparadora en los métodos del AVL y en las funciones de
+ * persistencia y recuperación del Indice.
+ */
+typedef struct {
+    int posicion;
+    int dni;
+    int edad;
+} ClaveEdadDNI;
+
+
+typedef int (*Accesor) (void *clave);
+typedef void (*Formulario) (void **clave);
+
+int compararClaveNombreDNI(void *clave1, void *clave2);
+int recuperarClaveNombreDNI(void **clave, FILE *archivo);
+int persistirClaveNombreDNI(void *clave, FILE *indice);
+void *crearClaveNombreDNI(Cliente *cliente, long posicionArchivo);
+void formularioClaveNombreDNI(void **clave);
+int obtenerPosicionClaveNombreDNI(void *clave);
+
+int compararClaveEdadDNI(void *clave1, void *clave2);
+int recuperarClaveEdadDNI(void **clave, FILE *archivo);
+int persistirClaveEdadDNI(void *clave, FILE *indice);
+void *crearClaveEdadDNI(Cliente *cliente, long posicionArchivo);
+void formularioClaveEdadDNI(void **clave);
+int obtenerPosicionClaveEdadDNI(void *clave);
 
 #endif // CLAVES_H_INCLUDED
